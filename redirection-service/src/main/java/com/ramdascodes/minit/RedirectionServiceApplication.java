08f697ed95ffa9ecda1064b58d8d69d0a58f9021
Ramdas Nair <ramdasnairhs@gmail.com>
2021-06-03 23:58:08 +0530

    Added redirection-service

---
+++ redirection-service/src/main/java/com/ramdascodes/minit/RedirectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ramdascodes.minit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectionServiceApplication.class, args);
+	}
+
+}
---
+++ redirection-service/src/test/java/com/ramdascodes/minit/RedirectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramdascodes.minit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
