cff35b2cd8534a1397177c2fca45d34622fb38cc
Ramdas Nair <ramdasnairhs@gmail.com>
2021-06-09 23:31:01 +0530

    Created new service key-generation-service

---
+++ key-generation-service/src/test/java/com/ramdascodes/minit/keygen/KeyGenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramdascodes.minit.keygen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyGenerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
